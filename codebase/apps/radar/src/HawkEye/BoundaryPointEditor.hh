70d10d02b351692570fd780b258c1980d94c3952
Jeff <jeff.s.smith@noaa.gov>
2019-10-30 17:50:38 -0600

    minor refactoring

--- codebase/apps/radar/src/HawkEye/BoundaryPointEditor.hh
+++ codebase/apps/radar/src/HawkEye/BoundaryPointEditor.hh
@@ -170,20 +170,54 @@ public:
 
 		// Special Cases
 		// p1, q1 and p2 are colinear and p2 lies on segment p1q1
-		if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+		if (o1 == 0 && onSegment(p1, p2, q1))
+			return true;
 
 		// p1, q1 and q2 are colinear and q2 lies on segment p1q1
-		if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+		if (o2 == 0 && onSegment(p1, q2, q1))
+			return true;
 
 		// p2, q2 and p1 are colinear and p1 lies on segment p2q2
-		if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+		if (o3 == 0 && onSegment(p2, p1, q2))
+			return true;
 
 		 // p2, q2 and q1 are colinear and q1 lies on segment p2q2
-		if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+		if (o4 == 0 && onSegment(p2, q1, q2))
+			return true;
 
 		return false; // Doesn't fall in any of the above cases
 	}
+
 private:
+
+	// Given three colinear points p, q, r, the function checks if
+	// point q lies on line segment 'pr'
+	static bool onSegment(Point &p, Point &q, Point &r)
+	{
+	    if (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) &&
+	    		q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y))
+	       return true;
+
+	    return false;
+	}
+
+	// To find orientation of ordered triplet (p, q, r).
+	// The function returns following values
+	// 0 --> p, q and r are colinear
+	// 1 --> Clockwise
+	// 2 --> Counterclockwise
+	static int orientation(Point &p, Point &q, Point &r)
+	{
+	    // See https://www.geeksforgeeks.org/orientation-3-ordered-points/
+	    // for details of below formula.
+	    float val = (q.y - p.y) * (r.x - q.x) -	(q.x - p.x) * (r.y - q.y);
+
+	    if (val == 0)
+	    	return 0;  // colinear
+
+	    return (val > 0)? 1: 2; // clock or counterclock wise
+	}
+
 	static void mergeClosestPoints(Point &p1, Point &p2, Point &p3, Point &p4)
 	{
 		float d12 = p1.distanceTo(p2);
@@ -217,33 +251,6 @@ private:
 		b.y = yMid;
 	}
 
-	// Given three colinear points p, q, r, the function checks if
-	// point q lies on line segment 'pr'
-	static bool onSegment(Point &p, Point &q, Point &r)
-	{
-	    if (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) &&
-	    		q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y))
-	       return true;
-
-	    return false;
-	}
-
-	// To find orientation of ordered triplet (p, q, r).
-	// The function returns following values
-	// 0 --> p, q and r are colinear
-	// 1 --> Clockwise
-	// 2 --> Counterclockwise
-	static int orientation(Point &p, Point &q, Point &r)
-	{
-	    // See https://www.geeksforgeeks.org/orientation-3-ordered-points/
-	    // for details of below formula.
-	    float val = (q.y - p.y) * (r.x - q.x) -	(q.x - p.x) * (r.y - q.y);
-
-	    if (val == 0)
-	    	return 0;  // colinear
-
-	    return (val > 0)? 1: 2; // clock or counterclock wise
-	}
 };
 
 
