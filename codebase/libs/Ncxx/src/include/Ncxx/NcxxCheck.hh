31e8fc597d5b8a4b44dfeab2555f271c7a881db9
dixon <dixon@ucar.edu>
2020-01-21 10:34:25 -0700

    libs/Ncxx, libs/Radx - removing #include <ncException.h> as it is no longer relevant and causes compiler errors

--- codebase/libs/Ncxx/src/include/Ncxx/NcxxCheck.hh
+++ codebase/libs/Ncxx/src/include/Ncxx/NcxxCheck.hh
@@ -48,7 +48,7 @@
 //////////////////////////////////////////////////////////////////////
 
 #include <stdio.h>
-#include <string.h>
+#include <string>
 #include <stdlib.h>
 
 #ifndef NcxxCheckFunction
