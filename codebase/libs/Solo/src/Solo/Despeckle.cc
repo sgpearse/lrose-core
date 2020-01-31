6860b3cee76dcbd15f1cbb740c68d6b91c83fe0d
Brenda Javornik <brenda@eol-albireo.eol.ucar.edu>
2020-01-30 17:36:30 -0700

    fixing despeckle; all unit tests pass except one

--- codebase/libs/Solo/src/Solo/Despeckle.cc
+++ codebase/libs/Solo/src/Solo/Despeckle.cc
@@ -1,10 +1,35 @@
+#include <cstdlib>
+#include <stdexcept>
+#include <string>
+
+// extern "C" is needed for python access to library
+// but this causes problems when linking from C++ code
+//extern "C"
+//{
+
+// A function adding two integers and returning the result
+int SampleAddInt(int i1, int i2)
+{
+  return i1 + i2;
+}
+
+  //
+  //  end points to first bad after length of good
+  // 
+void zap_speckle(size_t end, size_t length, float *data, float bad) {
+  size_t start = end - length;
+  for (size_t i = start; i<end; i++) 
+    data[i] = bad; 
+}
 
 
 //int se_despeckle(arg, cmds)             // #despeckle# 
 //int arg;
 //struct ui_command *cmds;
+// clip_gate determines the end of the ray
+// a_speckle determines the number of contiguous good data considered a speckle
 void se_despeckle(const float *data, float *newData, size_t nGates, float bad, int a_speckle,
-		  int dgi_clip_gate, short *boundary_mask) 
+		  int dgi_clip_gate, bool *boundary_mask) 
 {
   int below;
   //  struct ui_command *cmdq=cmds+1; // point to the first argument 
@@ -13,10 +38,10 @@ void se_despeckle(const float *data, float *newData, size_t nGates, float bad, i
   //  struct dds_structs *dds;
   //  char *dst_name;
 
-  int nc, nd, fn, bad, a_speckle;
+  int nc;
   int ii, nn, mark;
   float  *ss, *zz, *tt, *nss;
-  unsigned short *bnd;
+  bool *bnd;
 
 
   //  dst_name = (cmdq++)->uc_text;
@@ -30,8 +55,9 @@ void se_despeckle(const float *data, float *newData, size_t nGates, float bad, i
   //  a_speckle = seds->a_speckle;
   //  dgi = dd_window_dgi(seds->se_frame);
   //  dds = dgi->dds;
-  if (dgi_clip_gate >= nGates) throw new ArgumentException("dgi_clip_gate greater than number of gates");
-  nc = dgi_clip_gate+1;
+  //  if (dgi_clip_gate >= nGates) throw std::invalid_argument("dgi_clip_gate greater than number of gates");
+  if (dgi_clip_gate > nGates) throw std::invalid_argument("dgi_clip_gate greater than number of gates");
+  nc = dgi_clip_gate;
   bnd = boundary_mask;
   //                                                                                                             
   // find the field to be thresholded                                                                            
@@ -51,39 +77,92 @@ void se_despeckle(const float *data, float *newData, size_t nGates, float bad, i
   //bad = dds->parm[fn]->bad_data;
 
   // memcopy data into newData
-  memcpy(data, newData, nGates*sizeof(float));
+  //memcpy(newData, data, nGates*sizeof(float));
+  printf("\n");
+  for (size_t i = 0; i<nGates; i++) {
+    newData[i] = data[i];
+    printf("%f,", newData[i]);
+  }
+  printf("\n");
 
   size_t zzIdx = nGates;
   if (dgi_clip_gate < nGates)
-    zzIdx = dgi_clip_gate + 1;
+    zzIdx = dgi_clip_gate;
   //                                                                                                             
   // loop through the data                                                                                       
   //
+
   size_t ssIdx = 0;
+  size_t length = 0;
+  // accumulate length of good data until we hit a bad data;
+  // if the length is a speckle, zap it
+  // reset the length if we are out of boundary, and
+  // after zapping a speckle
+  //
   while (ssIdx < zzIdx) {
+    if (boundary_mask[ssIdx]) {
+      if ((newData[ssIdx] - bad) < 0.00001) {
+        if ((length > 0) && (length <= a_speckle)) {
+          size_t end = ssIdx;
+          zap_speckle(end, length, newData, bad);
+        }
+        length = 0;
+      } else { // good data
+        length += 1;
+      }
+    } else { // outside boundary
+      length = 0;
+    }
+    ssIdx += 1;
+    printf("ssIdx=%d length=%d\n", ssIdx, length);
+  }
+  // consider any ending length
+  bool at_end_of_ray = ssIdx >= nGates;
+  if ((length > 0) && (length <= a_speckle) && at_end_of_ray) {
+    size_t end = ssIdx;
+    zap_speckle(end, length, newData, bad);
+  }
+  
+  /*
+  size_t ssIdx = 0;
+  bool done = false;
+  while (!done) {  // (ssIdx < zzIdx) {
     // move to first good gate inside a boundary
     while (ssIdx < zzIdx && (data[ssIdx] == bad || !(boundary_mask[ssIdx]))) {
       ssIdx += 1;
     }
-    size_t nNotBad = 0;
-    size_t ttIdx = ssIdx;
-    // now move forward to the next bad flag                                                                   
-    //
-    while (ssIdx < zzIdx && boundary_mask[ssIdx] && data[ssIdx] != bad) {
-      nNotBad += 1;
-      ssIdx += 1;
-    } 
-    //(tt=ss,nn=0; ss < zz && *bnd && *ss != bad; nn++,ss++,bnd++);
-    if(boundary_mask[ssIdx] && nNotBad <= a_speckle) {
-      while (ttIdx < ssIdx) {
-        newData[ttIdx] = bad;      // zap speckle 
-        ttIdx+= 1;
-      }
-    }
-    // else,  not a speckle or outside boundary 
-
-
-  }
+    if (ssIdx >= zzIdx) {
+      done = true;
+    } else {
+      // now move forward to the next bad flag                                                                   
+      //
+      size_t nNotBad = 0;
+      size_t speckleStartIdx = ssIdx;
+      size_t speckleEndIdx = ssIdx;
+      while (ssIdx < zzIdx && boundary_mask[ssIdx] && data[ssIdx] != bad) {
+        nNotBad += 1;
+        ssIdx += 1;
+      } 
+      //(tt=ss,nn=0; ss < zz && *bnd && *ss != bad; nn++,ss++,bnd++);
+      // Why did we exit the loop?
+      // ssIdx could be past the end of the array
+      if (ssIdx >= zzIdx) {
+        done = true;
+      } else { 
+        // not at end of array; process this possible speckle 
+        // could be outside boundary
+        // found a good data value  ==> determine if the length
+     
+        if(boundary_mask[ssIdx] && nNotBad <= a_speckle) {
+          while (ttIdx < ssIdx) {
+            newData[ttIdx] = bad;      // zap speckle 
+            ttIdx+= 1;
+          } 
+≈ß        }
+      } // else not past the end of the array
+    } // else 
+  } // end while !done = past the end of the array
+  */
 
   /*
   for(; ss < zz; ss++,bnd++) {
@@ -102,3 +181,5 @@ void se_despeckle(const float *data, float *newData, size_t nGates, float bad, i
   return(fn);
   */
 }
+
+//}
